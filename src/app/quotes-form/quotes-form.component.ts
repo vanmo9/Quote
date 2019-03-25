08eae04a4e10ce1c043894dee17ed90f985ad080
mohassan <vanmowha@gmail.com>
2019-03-25 20:23:05 +0300

    add quotes

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -7,7 +7,7 @@ import {Quote} from '../quote';
   styleUrls: ['./quotes-form.component.css']
 })
 export class QuotesFormComponent implements OnInit {
-  newQuote= new Quote (0,"","",new Date());
+  newQuote= new Quote (0,"","","",new Date());
   @Output() addQuote=new EventEmitter<Quote>();
   submitQuote(){
     this.addQuote.emit(this.newQuote);
