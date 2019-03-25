dd621811e2b6751230925a05d670448c8e3174bd
mohassan <vanmowha@gmail.com>
2019-03-25 20:04:23 +0300

    add files

--- src/app/quotes-details/quotes-details.component.ts
+++ src/app/quotes-details/quotes-details.component.ts
@@ -1,36 +1,24 @@
-import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
-import {Quote} from '../quote'
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote'
 @Component({
   selector: 'app-quotes-details',
   templateUrl: './quotes-details.component.html',
   styleUrls: ['./quotes-details.component.css']
 })
 export class QuotesDetailsComponent implements OnInit {
-  
-  //deleting
-  @Input() quote:Quote;
-  @Output() isComplete= new EventEmitter<boolean>();
-  
-  
+
+
+  @Input() quote: Quote;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+
   downvote = 0;
   upvote = 0;
 
-  quoteDelete(complete:boolean){
+  quoteDelete(complete: boolean) {
     this.isComplete.emit(complete);
   }
-  
-/*
-  upVote(index){
-  
-    this.quote[index].upvote = ++this.quote[index].upvote
 
-  }
-  downVote(index){
-
-    this.quote[index].downvote = ++this.quote[index].downvote
-
-  }
-  */
 
   constructor() { }
 
