4dbee5287564b263fccaeb2fe636570c5855cd9f
mohassan <vanmowha@gmail.com>
2019-03-25 20:21:41 +0300

    add emitters

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit,  Output, EventEmitter } from '@angular/core';
 import {Quote} from '../quote';
+
 @Component({
   selector: 'app-quotes-form',
   templateUrl: './quotes-form.component.html',
