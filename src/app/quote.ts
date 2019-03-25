7b3a1bd02dd7409df1e6ff83bbbf034a44d2e495
mohassan <vanmowha@gmail.com>
2019-03-25 20:05:44 +0300

    add contructors

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,8 +2,8 @@ export class Quote {
     public upvote:number;
     public downvote:number;
     public showQuoteDetails:boolean;
-    constructor(public id:number, public name:string, public quote:string, public writtenDate:Date){
+    constructor(public id:number, public quote:string, public author:string, public writtenDate:Date){
         this.showQuoteDetails = false;
     }
-  
+
 }
